ff419f5b377315520b14a5ba804f48e2eb48ae50
Custromix <75646944+Custromix@users.noreply.github.com>
2026-05-03 16:58:54 +0900

    [ADD] Engine first commit

---
+++ CustroEngine/src/Shader.cpp
@@ -0,0 +1,102 @@
+#include "Shader.h"
+
+Shader::Shader(const char* vertexPath, const char* fragmentPath)
+{
+    std::string vertexCode, fragmentCode;
+    std::ifstream vShaderFile, fShaderFile;
+    
+    std::cout << "Vertex Shader : " << vertexPath << std::endl;
+    std::cout << "Fragment Shader : " << fragmentPath << std::endl;
+    
+    vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+    fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+    
+    try
+    {
+        vShaderFile.open(vertexPath);
+        fShaderFile.open(fragmentPath);
+        std::stringstream vShaderStream, fShaderStream;
+        std::cout << "Test : " << vShaderStream.str() << std::endl;
+        vShaderStream << vShaderFile.rdbuf();
+        fShaderStream << fShaderFile.rdbuf();
+        
+        vShaderFile.close();
+        fShaderFile.close();
+        
+        vertexCode = vShaderStream.str();
+        fragmentCode = fShaderStream.str();
+        
+    }catch (std::ifstream::failure& e)
+    {
+        std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ " << e.code() << std::endl;
+    }
+    
+    const char* vShaderCode = vertexCode.c_str();
+    const char* fShaderCode = fragmentCode.c_str();
+    
+    unsigned int vertexShaderID, fragmentShaderID;
+    int  success;
+    char infoLog[512];
+    
+    vertexShaderID = glCreateShader(GL_VERTEX_SHADER); //Créer un shader vide de type GL_VERTEX_SHADER
+    glShaderSource(vertexShaderID, 1, &vShaderCode, nullptr); //Assign le shader GLSL au vertexShader
+    glCompileShader(vertexShaderID); //Compile le vertex shader
+    
+    glGetShaderiv(vertexShaderID, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(vertexShaderID, 512, nullptr, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    
+    fragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER); //créer un shader vide de type GL_FRAGMENT_SHADER
+    glShaderSource(fragmentShaderID, 1, &fShaderCode, nullptr); //assign le shader GLSL au fragmentShader
+    glCompileShader(fragmentShaderID); //compile le fragmentShader
+    
+    glGetShaderiv(fragmentShaderID, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(fragmentShaderID, 512, nullptr, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    
+    ID = glCreateProgram();
+    glAttachShader(ID, vertexShaderID);
+    glAttachShader(ID, fragmentShaderID);
+    glLinkProgram(ID);
+    
+    
+    glGetProgramiv(ID, GL_LINK_STATUS, &success);
+    if(!success) {
+        glGetProgramInfoLog(ID, 512, nullptr, infoLog);
+        std::cout << "ERROR::SHADER_PROGRAM::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    
+    glDeleteShader(vertexShaderID);
+    glDeleteShader(fragmentShaderID);
+}
+
+void Shader::use()
+{
+    glUseProgram(ID);
+}
+
+void Shader::setBool(const std::string& name, bool value) const
+{
+    glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
+}
+
+void Shader::setInt(const std::string& name, int value) const
+{
+    glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+}
+
+void Shader::setFloat(const std::string& name, float value) const
+{
+    glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+}
+
+void Shader::setVec3(const std::string& name, float x, float y, float z) const
+{
+    glUniform3f(glGetUniformLocation(ID, name.c_str()), x, y, z);
+}
